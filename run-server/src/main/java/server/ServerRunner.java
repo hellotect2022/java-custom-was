92e69ec7774dd11b5fed9783591f97de0a6b59d1
hellotect2022 <hellotect2022@gmail.com>
2025-07-29 21:41:10 +0900

    서버 실행 객체 분리 ServerRunner

---
+++ run-server/src/main/java/server/ServerRunner.java
@@ -0,0 +1,48 @@
+package server;
+
+import config.ConfigLoader;
+import config.ServerConfig;
+import handler.ErrorPageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerRunner {
+    private final Logger logger = LoggerFactory.getLogger(ServerRunner.class);
+    private final ServerConfig serverConfig;
+    private final ErrorPageHandler errorPageHandler;
+    private final Map<String,String> servletMappings;
+    private final ExecutorService executorService;
+
+    public ServerRunner() {
+        // 1. 설정 로딩
+        this.serverConfig = ConfigLoader.getInstance().getServerConfig();
+        this.servletMappings = serverConfig.servlet_mappings;
+        this.errorPageHandler = new DefaultErrorPageHandler(serverConfig);
+        this.executorService = Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    public void start() throws IOException {
+        int port = serverConfig.port;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("[INFO] 서버 시작됨: 포트 "+port);
+            while (true) {
+                Socket client = serverSocket.accept();
+                RequestProcessor processor = new RequestProcessor(client, errorPageHandler);
+                executorService.execute(processor);
+            }
+        }catch (IOException ex) {
+            logger.error("Error accepting connection", ex);
+        }
+    }
+
+    public void stop() {
+        executorService.shutdown();
+    }
+}
