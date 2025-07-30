79809eb1359659a7acd5c4c526ec2eeeaf2ebc5d
hellotect2022 <hellotect2022@gmail.com>
2025-07-30 12:53:38 +0900

    Config 코드 및 RequestProcessor 로직을 was-core 로 이동 Banner.txt 파일 추가(파일로딩시에 프로그램 제목 출력 용)

--- run-server/src/main/java/HttpServerApplication.java
+++ run-server/src/main/java/HttpServerApplication.java
@@ -10,6 +10,7 @@ import server.RequestProcessor;
 import server.ServerRunner;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 
 public class HttpServerApplication {
@@ -18,12 +19,27 @@ public class HttpServerApplication {
 
     public static void main(String[] args) throws Exception {
         logger.debug("DHhan Boot init!!");
+        printBannerFromResource();
 
         try {
             ServerRunner runner = new ServerRunner();
             runner.start();
         } catch (IOException ex) {
             logger.error("Server could not start", ex);
+        }
+    }
+
+    private static void printBannerFromResource() {
+        try (InputStream is = HttpServerApplication.class.getClassLoader().getResourceAsStream("banner.txt")) {
+            if (is != null) {
+                String banner = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                System.out.println(banner);
+            } else {
+                System.out.println(":: DHHAN BOOT ::");
+            }
+        } catch (IOException e) {
+            System.out.println(":: DHHAN BOOT :: (banner load failed)");
+            e.printStackTrace();
         }
     }
 }
